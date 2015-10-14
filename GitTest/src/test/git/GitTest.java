ef0d09bb1d9c77a8b9cbc222c51e1b25cd00e756
zwt <test@test.com>
2015-10-14 14:02:16 +0800

    add file test

--- GitTest/src/test/git/GitTest.java
+++ GitTest/src/test/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
 	public static void main(String[] args){
 		System.out.println("Git test");
 		System.out.println("modify");
+		System.out.println("modify2");
 	}
 }
---
+++ GitTest/src/test/git/Test.java
@@ -0,0 +1,9 @@
+package test.git;
+
+public class Test {
+	public static void main(String[] args){
+		System.out.println("Test");
+		System.out.println("Test modify");
+		System.out.println("Test modify2");
+	} 
+}
